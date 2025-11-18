7f3484e04bfd21024791fae4a42bc8a4943b87f4
Marvin Friedrich <contact@marvinf.com>
2025-11-18 18:25:58 +0100

    tasks/empty-image: Use sparse files for images

--- tasks/empty-image.sh
+++ tasks/empty-image.sh
@@ -7,18 +7,18 @@ IMAGE_SIZE="$2"
 ESP_SIZE="$3"
 
 # Create an empty disk image
-dd if=/dev/zero of="$OUTPUT_IMAGE" bs=1G count=${IMAGE_SIZE%%G}
-
-# Create partitions
-parted -s "$OUTPUT_IMAGE" mklabel gpt
-parted -s "$OUTPUT_IMAGE" mkpart ESP fat32 1MiB ${ESP_SIZE}
-parted -s "$OUTPUT_IMAGE" set 1 esp on
-parted -s "$OUTPUT_IMAGE" mkpart ROOT ext4 ${ESP_SIZE} 100%
+truncate -s $IMAGE_SIZE "$OUTPUT_IMAGE"
 
 # Setup loop device
-LOOPDEV=$(sudo losetup --find --show --partscan "$OUTPUT_IMAGE")
+LOOPDEV=$(sudo losetup --find --show "$OUTPUT_IMAGE")
 ESP_PART="${LOOPDEV}p1"
 ROOT_PART="${LOOPDEV}p2"
+
+# Create partitions
+sudo parted -s "$LOOPDEV" mklabel gpt
+sudo parted -s "$LOOPDEV" mkpart ESP fat32 1MiB ${ESP_SIZE}
+sudo parted -s "$LOOPDEV" set 1 esp on
+sudo parted -s "$LOOPDEV" mkpart ROOT ext4 ${ESP_SIZE} 100%
 
 # Format partitions
 sudo mkfs.vfat -F 32 "$ESP_PART"
